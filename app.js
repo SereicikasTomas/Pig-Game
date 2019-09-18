1d9e80c666958948852c63db8626879881bf88a3
jacando.tos <sereicikas.t@gmail.com>
2019-09-18 10:28:56 +0200

    hide the dice on load

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ let activePlayer = 0;
 let dice = Math.ceil(Math.random() * 6); // Gets random number from 1 to 6
 
 document.querySelector(`#current-${activePlayer}`).textContent = dice;
+
+document.querySelector(".dice").style.display = "none";
