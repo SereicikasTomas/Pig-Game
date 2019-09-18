963579a0f54b13479b5b4703e117d5e24ae949f8
jacando.tos <sereicikas.t@gmail.com>
2019-09-18 18:25:46 +0200

    add eevent for dice roll

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 let scores = [0, 0];
 let roundScore = 0;
 let activePlayer = 0;
-let dice = Math.ceil(Math.random() * 6); // Gets random number from 1 to 6
-
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
 
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  let dice = Math.ceil(Math.random() * 6);
+
+  let diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = `dice-${dice}.png`;
+
+  if (dice === 1) {
+    activePlayer = 1;
+  }
+  console.log(dice);
+});
